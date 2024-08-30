1dc6871d7c40b53a4823649642e3df0be6fcab07
Gaurav Khandelwal <110447594+GauravKh01@users.noreply.github.com>
2024-08-30 19:14:41 +0530

    Update stats.cpp
    updated

--- stats.cpp
+++ stats.cpp
@@ -4,7 +4,7 @@ Stats Statistics::ComputeStatistics(const std::vector<double>& vec) {
     //Implement statistics here
     Stats objStat;
     objStat.min= *min_element(vec.begin(), vec.end());
-    objStat.max= *max_element(vec.begin(), vec.end())
+    objStat.max= *max_element(vec.begin(), vec.end());
     objStat.average = accumulate( vec.begin(), vec.end(), 0.0)/vec.size();
 
     return objStat;
